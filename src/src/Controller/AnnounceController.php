588aba2fc0d4ff755c4d83095ee73f3aac6a2712
dbnsf <d_bruneau2@hetic.eu>
2022-10-26 14:37:39 +0200

    get announce by id (single page)

--- src/src/Controller/AnnounceController.php
+++ src/src/Controller/AnnounceController.php
@@ -71,18 +71,18 @@ class AnnounceController extends AbstractController
 
     // #[Route('/userannounces', name: 'app_userannounces')]
     
-    // #[Route('/announce/{id}', name: 'app_announcebyid')]
-    // public function getAnnounceById(int $id, EntityManagerInterface $entityManager): Response
-    // {
-    //     // $loggedUser = $this->getUser()->getId();
-    //     // $announce = $entityManager->getReference(Announces::class, $id);
-    //     $repository = $entityManager->getRepository(Announces::class);
-    //     $announce = $repository->find($id);
-    //     return $this->render('announce/single.html.twig', [
-    //         'announce' => $announce,
-    //         // 'userId' => $loggedUser
-    //     ]);
-    // }
+    #[Route('/announce/{id}', name: 'app_announcebyid')]
+    public function getAnnounceById(int $id, EntityManagerInterface $entityManager): Response
+    {
+        // $loggedUser = $this->getUser()->getId();
+        // $announce = $entityManager->getReference(Announces::class, $id);
+        $repository = $entityManager->getRepository(Announces::class);
+        $announce = $repository->find($id);
+        return $this->render('announce/single.html.twig', [
+            'announce' => $announce,
+            // 'userId' => $loggedUser
+        ]);
+    }
 
     #[Route('/getannounce{id}', name: 'app_getannounce')]
     public function get(int $id, EntityManagerInterface $entityManager){
