13cd31d64a239f9f413fc3ea360b495a786bb56c
Vince <mvincem@gmail.com>
2022-10-30 22:25:50 +0100

    fixture create admin

--- src/src/DataFixtures/AppFixtures.php
+++ src/src/DataFixtures/AppFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Tags;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -14,6 +15,14 @@ class AppFixtures extends Fixture
         $sndTag = new Tags();
         $trdTag = new Tags();
 
+        $user = new User();
+
+        $user->setEmail('admin@admin.com')
+            ->setName('admin')
+            ->setPassword('admin!')
+            ->setRoles(array('ROLE_ADMIN'))
+            ->setCreated_At(new \DateTimeImmutable('now'));
+
         $firstTag->setName("Immobilier");
         $sndTag->setName("Vacance");
         $trdTag->setName("Vehicule");
@@ -21,6 +30,7 @@ class AppFixtures extends Fixture
         $manager->persist($firstTag);
         $manager->persist($sndTag);
         $manager->persist($trdTag);
+        $manager->persist($user);
 
         $manager->flush();
 
